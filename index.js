722bf76386d8e643747e7d0566fc2afc4e591899
Qaiser Shehzad <qaiser@Qaisers-MacBook-Pro.local>
2019-11-19 17:49:43 +0500

    error handling added with nodemon and morgan

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.use(morgan('dev'));
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+
+});
 module.exports = app;
 
 
