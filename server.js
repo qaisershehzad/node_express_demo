22ac7639153b478d5198f8205fb9a8d1aeb0659c
Qaiser Shehzad <qaiser@Qaisers-MacBook-Pro.local>
2020-01-07 16:24:41 +0500

    added environment variable support for more variables

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const http = require('http');
 const app = require('./index');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
 // PORT
 server.listen(port, () => {
   console.log(`Listening ${port}`);
